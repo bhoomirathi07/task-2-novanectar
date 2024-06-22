35e8ac593c8c54c1b1a0f267f46be36428a60f98
bhoomirathi07 <bhoomirathi2299@gmail.com>
2024-06-22 11:58:28 +0530

    Add files via upload

---
+++ seeds/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const shopNowButton = document.querySelector('#hero button');
+
+    shopNowButton.addEventListener('click', function() {
+        console.log('Shop Now button clicked!');
+        // Add your logic here for what should happen when the button is clicked
+    });
+});
